0f555e626e358f751e901d9d5f447e00780308bd
MarioDonna <mariodonna23@gmail.com>
2020-03-18 11:02:31 -0600

    Test Files

---
+++ 08 Errors and Exceptions/cap.py
@@ -0,0 +1,10 @@
+def cap_text(text):
+    """
+    Input a string
+    Output the capitalized string
+    """
+    #capitaliza() only capitalizes first letter of the first word
+    #title() capitalizes first letter of every word
+
+    #return text.capitalize()
+    return text.title()
